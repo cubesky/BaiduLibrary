efef46ef7e8bab958715e0306ebb2470f0c22743
cubesky <max_3011@qq.com>
2015-12-12 00:01:34 +0800

    Create BaiduException.java

---
+++ BaiduException.java
@@ -0,0 +1,18 @@
+public class BaiduException extends Throwable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 6355312250297922862L;
+
+	@Override
+	public String getMessage() {
+		return "Baidu Exception";
+	}
+
+	@Override
+	public String toString() {
+		return "Baidu Exception";
+	}
+
+}
